def2b5e185e365d2687ee8cfba4ff706eed404cb
Jayashanka  Deshan <maxwon555@gmail.com>
2022-12-31 22:17:25 +0530

    add updated index.html file and app.js file

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const colorPicker = document.querySelector("#color-picker");
 
 const colorList = document.querySelector(".all-colors");
 
+const clearAll = document.querySelector(".clear-all");
+
 const pickedColors = JSON.parse(localStorage.getItem("color-data") || "[]");
+
+const copyColor = elem => {
+
+    navigator.clipboard.writeText(elem.dataset.color);
+
+    //elem.innerText = "Copied";
+
+    //setTimeout(() => elem.innerText = elem.dataset.color, 1000);
+}
 
 const showColors = () => {
 
@@ -15,10 +26,17 @@ const showColors = () => {
           
           border: 1px solid ${color == "#FFFFFF" ? "#ccc" : color}"></span>
                 
-          <span class="value">${color}</span>
+          <span class="value" data-clor="${color}">${color}</span>
             
        </li>
-    `).join("")
+    `).join("");
+
+    // add a click event listener to each color element to copy the color code
+
+    document.querySelectorAll(".color").forEach(li => {
+
+        li.addEventListener("click", e => copyColor(e.currentTarget.lastElementChild));
+    });
 
 
 }
@@ -47,5 +65,15 @@ const  activateEyeDropper = async () => {
         console.log(error)
     }
 }
+
+const clearAllColors = () => {
+
+    pickedColors.length = 0;
+
+    localStorage.setItem("color-data", JSON.stringify(pickedColors));
+
+}
+
+clearAll.addEventListener("click", clearAllColors);
 
 colorPicker.addEventListener("click", activateEyeDropper);
